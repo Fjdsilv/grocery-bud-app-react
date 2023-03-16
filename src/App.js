3a5d9ab33cfe575e1b53abaf8a9b16225ba6b168
felipe silva <fjdsilv.dev@protonmail.com>
2023-03-16 10:40:07 -0300

    local storage ok

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import { useState, useEffect } from 'react';
 import List from './components/List';
 import Alert from './components/Alert';
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list) {
+    return JSON.parse(localStorage.getItem('list'))
+  }
+  else {
+    return []
+  }
+}
+
 function App() { 
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({show: false, msg: '', type: ''});
@@ -62,6 +72,10 @@ function App() {
     setEditID(id)
     setName(specificItem.title);
   }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
 
   return (
     <div>
